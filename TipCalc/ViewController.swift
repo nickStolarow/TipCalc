eb70550d4ff5b217be9a9322b1b4e6591d8dcb38
Nick Stolarow <nickstolarow@gmail.com>
2020-07-25 23:58:19 -0600

    Nav bar from Tip Calculator to Tip Calc

--- TipCalc/ViewController.swift
+++ TipCalc/ViewController.swift
@@ -10,11 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var groupField: UITextField!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var perPersonLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    // Dismisses keyboard when screen is tapped
+    @IBAction func onTap(_ sender: Any) {
+        view.endEditing(true)
+    }
+    
+    @IBAction func calculateTip(_ sender: Any) {
+        //Get data from fields
+        let bill = Double(billField.text!) ?? 0
+        let group = Double(groupField.text!) ?? 1
+        // Computations
+        let tipPercentages = [0.15, 0.18, 0.2]
+        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        let total = bill + tip
+        let perPerson = total / group
+        // Update Labels
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+        perPersonLabel.text = String(format: "$%.2f", perPerson)
+    }
 }
-
